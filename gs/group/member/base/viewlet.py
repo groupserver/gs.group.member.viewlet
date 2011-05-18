8422b8dac6fa94ca1fc1cdba76aad557fd663db7
Michael JasonSmith <mpj17@onlinegroups.net>
2011-05-18 17:03:57 +1000

    Adding the viewlet classes from gs.group.base and gs.group.home

---
+++ gs/group/member/base/viewlet.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from Products.XWFCore.XWFUtils import getOption
+from utils import user_admin_of_group, user_group_admin_of_group, \
+    user_division_admin_of_group
+from gs.group.base.viewlet import GroupViewlet
+
+class GroupAdminViewlet(GroupViewlet):
+    def __init__(self, group, request, view, manager):
+        GroupViewlet.__init__(self, group, request, view, manager)
+        
+    @Lazy
+    def isAdmin(self):
+        retval = not(self.loggedInUser.anonymous) \
+                    and user_admin_of_group(self.loggedInUser,
+                                            self.groupInfo)
+        return retval
+        
+    @Lazy
+    def isSiteAdmin(self):
+        retval = user_division_admin_of_group(self.loggedInUser,
+                                                self.groupInfo)
+        return retval
+
+    @Lazy
+    def isGroupAdmin(self):
+        retval = user_group_admin_of_group(self.loggedInUser,
+                                            self.groupInfo)
+        return retval
+
+    @Lazy
+    def show(self):
+        retval = self.isAdmin
+        assert type(retval) == bool
+        return retval
+
+class SiteAdminViewlet(GroupAdminViewlet):
+    def __init__(self, group, request, view, manager):
+        GroupAdminViewlet.__init__(self, group, request, view, manager)
+
+    # TODO: Sort out the posting limit mess
+    @Lazy
+    def canSetPostingLimit(self):
+        retval = self.isSiteAdmin and \
+            getOption(self.context, 'admin_can_set_posting_limit', True)
+        return retval
+
+    @Lazy
+    def show(self):
+        retval = not(self.loggedInUser.anonymous) and self.isSiteAdmin
+        assert type(retval) == bool
+        return retval
+
